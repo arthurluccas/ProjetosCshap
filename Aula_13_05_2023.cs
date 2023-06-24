fac2f2ae423ad7ef8465704e14bcefd87dffa498
ARTHUR LUCAS SILVA <arthur.lsilva10@senacsp.edu.br>
2023-06-24 10:36:51 -0300

    Aula08

---
+++ Aula_13_05_2023.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* var nota = 5;
+             switch (nota)
+             {
+                 case 1:
+                     Console.WriteLine("Nota foi pessima");
+                     break;
+
+
+
+                 case 2:
+                     Console.WriteLine("Nota foi ruim");
+                       break;
+                 case 3:
+                     Console.WriteLine("Nota foi regular");
+                     break;
+
+                 case 4:
+                     Console.WriteLine("Nota foi bom");
+                     break;
+
+                 case 5:
+                     Console.WriteLine("Nota foi ótimo");
+                     break;
+
+                 default: Console.WriteLine("Você não foi avaliado");
+                     break;
+             } */
+
+
+
+            int n1 = 15;
+            int n2 = 10;
+
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            /*double nota = 6;
+            double nota2 = 9;
+
+            var res = nota + nota2 / 2 >= 7;
+            Console.WriteLine($"O aluno está aprovado");*/
+
+
+
+            Console.WriteLine("Digite seu nome:");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Digite seu peso em kg:");
+            double peso = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite sua altura:");
+            double altura = double.Parse(Console.ReadLine());
+
+            double imc = peso / altura * altura;
+
+            Console.WriteLine($"Olá {nome}Seu IMC {imc.ToString()} é: ");
+
+            if (imc >= 18.5)
+            {
+                Console.WriteLine("IMC está dentro do intervalo saudável");
+
+
+            }
+            else (imc <= 25)
+             {
+                Console.WriteLine("IMC não está saudável");
+            }    
+
+            
+
+
+
+
+        }
+    }
+}
